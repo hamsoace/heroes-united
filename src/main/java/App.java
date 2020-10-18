f4a2c72a91807cbf0e4cf551ca755211d761a9d6
hamsoace <hamsoace@gmail.com>
2020-10-18 20:42:14 +0300

    created heroes object

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,21 @@
+import models.Heroes;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args){
+        Heroes heroes;
+        heroes = new Heroes("Batman", 40, "gadgets+intellect", "parents' death");
 
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("Heroes", Heroes.heroes);
+            return new ModelAndView(model, "heroinfo.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
--- src/main/java/models/Heroes.java
+++ src/main/java/models/Heroes.java
@@ -1,4 +1,27 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Heroes {
+    String name;
+    int age;
+    String superPower;
+    String weakness;
+
+    public static List<Heroes> heroes = new ArrayList<>();
+
+    public Heroes(String name, int age, String superPower, String weakness){
+        this.name = name;
+        this.age = age;
+        this.superPower = superPower;
+        this.weakness = weakness;
+        heroes.add(this);
+    }
+    public String heroName(){
+        return name;
+    }
+    public String heroPower(){
+        return superPower;
+    }
 }
