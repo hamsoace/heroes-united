4fd6bf6f5ace41a4173ff588e166fed048b0e8d9
hamsoace <hamsoace@gmail.com>
2020-10-21 13:59:25 +0300

    implement DAO

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,13 +9,6 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args){
-        Heroes heroes;
-        heroes = new Heroes("Batman", 40, "gadgets+intellect", "parents' death");
-
-        get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("Heroes", Heroes.heroes);
-            return new ModelAndView(model, "heroinfo.hbs");
-        }, new HandlebarsTemplateEngine());
+        System.out.println("Iam OK!");
     }
 }
--- src/main/java/models/Heroes.java
+++ src/main/java/models/Heroes.java
@@ -4,24 +4,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Heroes {
-    String name;
-    int age;
-    String superPower;
-    String weakness;
+    private int id;
+   private String name;
+   private int age;
+   private String superPower;
+   private String weakness;
+   private int teamId;
 
-    public static List<Heroes> heroes = new ArrayList<>();
 
-    public Heroes(String name, int age, String superPower, String weakness){
+    public Heroes(String name, int age, String superPower, String weakness, int teamId) {
         this.name = name;
         this.age = age;
         this.superPower = superPower;
         this.weakness = weakness;
-        heroes.add(this);
+        this.teamId = teamId;
     }
-    public String heroName(){
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
         return name;
     }
-    public String heroPower(){
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSuperPower() {
         return superPower;
+    }
+
+    public void setSuperPower(String superPower) {
+        this.superPower = superPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
     }
 }
