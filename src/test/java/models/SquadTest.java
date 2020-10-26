eeae33175f1247649a51be58c8d60bf2c335745b
hamsoace <hamsoace@gmail.com>
2020-10-26 20:44:53 +0300

    squad test

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,7 +1,25 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SquadTest {
+    Squad squad = new Squad("Avengers", "avenging the universe", 1);
+
+    @Test
+    public void getSquadName(){
+        assertEquals("Avengers", squad.getSquadName());
+    }
+
+    @Test
+    public void getCause(){
+        assertEquals("avenging the universe", squad.getSquadCause());
+    }
+
+    @Test
+    public void getSquadSize(){
+        assertEquals(1, squad.getSquadSize());
+    }
 
 }
